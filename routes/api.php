fdea87d630c8e76820b5fa4205b8c9c737d69c92
Jack Noordhuis <jackjnoordhuis@gmail.com>
2019-03-19 15:28:10 +1100

    GitHub webhook route, just log incoming requests for now

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'webhook', 'as' => 'webhook.'], function () {
+    Route::group(['prefix' => 'github', 'as' => 'github'], function () {
+        Route::post('/', function (Request $request) {
+            Log::debug($request);
+        });
+    });
+
+});
